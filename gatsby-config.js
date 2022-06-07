69ffb579dad89931fceb7507234e70311891f825
Andre Richardi <a.richardi@gmail.com>
2022-06-07 08:20:59 -0300

    plugins and basic configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Andre Richardi homesite',
+    description: 'My personal portfolio and blog',
+    contact: 'a.richardi@gmail.com'
+  }
 }
