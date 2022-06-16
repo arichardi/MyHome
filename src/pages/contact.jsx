5eb0062edebc56c362c14324d93ec0d5b9d50081
Andre Richardi <a.richardi@gmail.com>
2022-06-16 11:06:05 -0300

    contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { container, line, graphic} from '../styles/contact.module.css'
+import { container, line, myimage} from '../styles/contact.module.css'
 import HeaderApp from '../components/HeaderApp'
 import ExternalLinks from '../components/ExternalLinks'
+import ContactGraph from '../SVG/ContactGraph'
 
 export default function contact() {
+
+
+
     return (
         <div className={container}>
             <HeaderApp />
             <h1>Contato</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem laboriosam vel quas totam, ducimus illum error ratione voluptate modi iste nobis? Similique asperiores consequatur ullam temporibus ea cumque rem repellat!</p>
-            <div className={line}></div>
-                <ExternalLinks />
-            <div className={graphic}>
-                <img src="/contactIMG.png" alt="background" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget neque purus. Maecenas accumsan interdum mi quis tempus. Quisque sed fringilla nunc. Nulla vitae hendrerit velit. Phasellus vu lputate rhoncus leo, id dapibus quam pellentesque eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <div className={line}/>
+            <ExternalLinks />
+            <div className={myimage}>
+                <ContactGraph />
             </div>
         </div>
     )
